1cedf69909b4bfdafea42fb2c66360bda1f708fc
Akashiutchiha <christianhonore2003@gmail.com>
2022-02-28 22:29:48 +0100

    day 1

--- Intro.dart
+++ Intro.dart
@@ -1,4 +1,3 @@
 main() {
-  print(
-      "My mamma always said, life is like a box of choclates. You never know what you're gonna get.");
+  print("My mamma always said, life is like a box of choclates. You never know what you're gonna get.");
 }
